cf67d768d23593ae46d718d39482882663f589e0
rickyl3aks <65684689+rickyl3aks@users.noreply.github.com>
2021-01-03 00:56:13 +0000

    score

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ function checkForMatch() {
   cardsChosenId = [];
   resultDisplay.textContent = cardsWon.length;
   if (cardsWon.length === cardArray.length / 2) {
-    resultDisplay.textContent = "Congratulations! You found them all! 💯";
+    resultDisplay.textContent = "You Win!! 🥳 💯";
   }
 }
 
